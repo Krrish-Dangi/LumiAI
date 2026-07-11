f7ef1137117a70fe56d2e566826039b68c6a88d6
Krrish-Dangi <krrishdangi2005@gmail.com>
2026-07-11 10:55:56 +0530

    Fixed some bugs and completed missing rag in chat.py

--- backend/api/chat.py
+++ backend/api/chat.py
@@ -7,12 +7,15 @@ from langchain_core.runnables.history import RunnableWithMessageHistory
 from services.history import get_history
 from store.session_store import history_store, vector_store, uploaded_files
 from langchain_core.messages import get_buffer_string
+from langchain_core.runnables import RunnablePassthrough, RunnableLambda
+from operator import itemgetter
 
 chat_router = APIRouter()
 
 @chat_router.post("/chat")
 def start_chat(mssg: ChatRequest, session_id: SessionArtifacts):
     llm = get_groq_model("llama-3.3-70b-versatile")
+
 
     prompt = ChatPromptTemplate.from_messages([
         ("system", 
@@ -28,11 +31,21 @@ def start_chat(mssg: ChatRequest, session_id: SessionArtifacts):
             4. Fallback to General Knowledge: If the provided documents do not contain the answer, or if no documents are provided, you may rely on your general training data to answer the question. If you do this, make it clear that you are drawing from general knowledge rather than the retrieved files.
             5. Do Not Hallucinate: Never invent facts, data, or quotes that are not supported by the context or your established knowledge base. Keep your tone friendly, concise, and direct.'''
         ),
+        ("system", "Context of user uploaded document:\n\n {context}"),
         MessagesPlaceholder(variable_name="history"),
         ("user", "{question}")
     ])
 
-    chain = prompt|llm
+    db = vector_store.get(session_id.id)
+    if db == None:
+        chain = RunnablePassthrough.assign(
+            context=RunnableLambda(lambda _: "")
+        )|prompt|llm
+    else:
+        retriever = db.as_retriever()
+        chain = RunnablePassthrough.assign(
+            context=itemgetter("question") | retriever
+        )|prompt|llm
 
     chat_with_history = RunnableWithMessageHistory(runnable=chain, get_session_history= get_history, input_messages_key="question", history_messages_key="history")
     response = chat_with_history.invoke(input= {
@@ -65,5 +78,5 @@ def get_session_history(session_id: str):
         res = get_buffer_string(message_history.messages)
         return Response(status_code=200, content= res)
     
-    return HTTPException(status_code=404, detail= f"{session_id} not found...")
+    raise HTTPException(status_code=404, detail= f"{session_id} not found...")
         
